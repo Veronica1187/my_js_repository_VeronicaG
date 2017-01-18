7366c837f9f47de757bbe59b745b843bb24c821c
Veronica1187 <veronica.garcia1187@gmail.com>
2017-01-18 14:45:03 -0600

    Add files via upload

---
+++ Practica1_VeronicaGarcia.js
@@ -0,0 +1,45 @@
+//PRACTICA 1 JAVASCRIPT
+
+//Imprimir al menos 10 lineas con una frase
+
+Console.log("Mi primer ejercicio de javascript");
+console.log("Está");
+console.log("Sencillo");
+console.log("Pero");
+console.log("Quiero");
+console.log("Aprender");
+console.log("Más");
+console.log("y");
+console.log("Más");
+console.log("Cosas Nuevas");
+
+//Declarar 5 arreglos
+var Mujeres= ["Veronica", "Ana", "Natalia"];
+var = Week["Mon", "Tue", "Wed","Thu", "Fri"];
+var = Months["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
+var = Animals["Cat", "Dog", "Snake", "Bird"];
+var = Celphones["iPhone", "Nexus5", "Motox"];
+
+
+//Tamaño de las palabras
+
+console.log (Mujeres.length);
+console.log (Months.length);
+console.log (Animals.length);
+console.log (Celphones.length);
+console.log ("Veronica").length;
+
+//Declarar 5 variables
+
+Var Name= 'Veronica';
+Var Age= 29;
+Var Hobbie= 'Bailar';
+Var Role= 'Software Tester'
+Var Color='Morado'
+
+//Formular 5 preguntas a usuarios
+var Question1= prompt("What is your name?");
+var Question2= prompt("Where are you from?");
+var Questio3= prompt("How old are you?");
+var Question4= prompt("What is your favorite color");
+var Question5= prompt("What is your hobbie?");
