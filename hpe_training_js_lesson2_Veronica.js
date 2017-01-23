21296e9fdb05de940759be8e32d75c6e70d5c683
Veronica1187 <veronica.garcia-martinez@hpe.com>
2017-01-23 15:33:49 -0600

    Practica2
    nueva practica

---
+++ hpe_training_js_lesson2_Veronica.js
@@ -0,0 +1,120 @@
+//6 comparisons
+
+Console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2 === 16);
+Console.log("Veronica".length > 12);
+console.log("Good Morning".length < 8);
+console.log(8*2 === 10);
+
+//Substrings (0,2), (2,3), (0,5), (1,3), (5,7),
+
+"veronica".substring(2,3);
+"hello". substring(0, 2);
+"Batman".substring(0,5);
+"laptop".substring(1,3);
+"wonderful day".substring(5,7);
+
+//5 arrays and print only one of them
+
+var mujeres= ["Veronica", "Ana", "Natalia"];
+console.log (mujeres[2]);
+
+var = week["Mon", "Tue", "Wed","Thu", "Fri"];
+console.log (week[4]);
+
+var = months["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
+console.log (months[3]);
+
+var = animals["Cat", "Dog", "Snake", "Bird"];
+console.log (animals[0]);
+
+var = celphones["iPhone", "Nexus5", "Motox"];
+console.log (celphones[1]);
+
+
+
+//Write 6 simple if's
+
+
+//#1
+
+if ("Natalia".length !== 8) {
+    console.log("I'm in the right way" );
+}
+
+
+//#2
+if ("Natalia".length === 9 )
+{
+    console.log("Let's go down the first road!");
+}
+else
+{
+   console.log("You're my Angel"); // What should we do if the condition is false? Fill in here:
+
+}
+
+
+//#3
+if("vero".length === 48)
+{
+
+console.log ("The cindition is true");
+}
+    else
+
+    {
+        console.log("The condition is false");
+
+        }
+
+
+//#4
+
+if (10 === 10)
+{
+    console.log("You got a true!");
+}
+else {
+    console.log("You got a false!");
+}
+
+
+//#5
+
+
+if (12 / 4 === "Ari".length)
+{
+    confirm("Will this run the first block?");
+}
+
+else
+{
+    confirm("Or the second block?");
+}
+
+
+
+//#6
+
+if ("Jon".length * 2 / (2+1) === 2)
+{
+    console.log("The answer makes sense!");
+}
+else
+{
+
+
+  //write 6 sentences where 3 of them returns 0 as result and 3 others with 1 as the output
+
+
+//Returns 0
+console.log(10000 % 100);
+console.log (9999 % 9);
+console.log(11000 % 10);
+
+//Returns 1
+console.log(11000 % 10999);
+console.log (9999 % 9998);
+console.log (1000 % 999);
